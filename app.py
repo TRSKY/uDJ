81471d90f4220614494b37fe01acb93f82a18493
Alex <alex.mutricy@berkeley.edu>
2016-11-12 22:29:41 -0800

    added login test endpoint and groundwork for backend

--- app.py
+++ app.py
@@ -1,12 +1,68 @@
 import json
 import os
+import re
 from flask import Flask, render_template, request, redirect, url_for, abort, Response
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.test
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/test/insertdb/user", methods=['POST'])
+def insertTest():
+	print 'we get here'
+	key1 = "username"
+	key2 = "device_ID"
+	user = request.form['user']
+	#user should have the correct schema {"username":"jdoe13", "device_ID": "xxxxx"}
+	user = json.loads(user)
+	if len(user.keys()) != 2:
+		return json.dumps("error")
+	if key1 in user.keys() and key2 in user.keys():
+		if re.match("^[a-zA-Z0-9_]+$", user[key1]) != None and re.match("^[a-zA-Z0-9_]+$",user[key2]) != None:
+			result = db.users.insert_one(user)
+			print result.inserted_id
+			print type(result.inserted_id)
+			return json.dumps(str(result.inserted_id))
+	return "error"
+
+@app.route("/test", methods=['GET', 'POST'])
 def index():
 	return render_template('index.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	#logs in existing usr, makes new user if the request is valid and the usr cannot be found
+	return render_template('login.html')
+
+@app.route("/<user_id>", methods=['GET'])
+def user_page(user_id):
+	#we need a usr db
+	#get usr meta from usr db
+	#return user meta as json
+	return ''
+
+@app.route("/event", methods=['GET', 'POST'])
+def new_event(event_id):
+	#create new event 
+	#return the new event id
+	return ''
+
+@app.route('/events/<query>', methods=["GET, POST"])
+def search_events(query):
+	#return events matched to query
+	return ''
+
+@app.route("/event/<event_id>", methods=['GET', 'PUT'])
+def event(event_id):
+	#add songs to queue
+	return ''
+
+@app.route("/admin/event/<event_id>", methods=['GET', 'PUT'])
+def admin(event_id):
+	#check if the request is actually from the admin
+	#admin power behavior
+	return ''
 
 @app.errorhandler(404)
 def page_not_found(e):
--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,62 @@
-var { Router, Route, IndexRoute, Link, browserHistory } = ReactRouter
-
 var Main = React.createClass({
 	getInitialState: function() {
 		return {title: "Skeleton Code"}
 	},
 	render: function() {
 		return (
-			<div className="container">
-				<h1 className="title">{this.state.title}</h1>
+			<div>
+				<div className="container">
+					<h1 className="title">{this.state.title}</h1>
+				</div>
+				<TestUserCreate/>
 			</div>
+
 		)
 	}
 });
+
+var TestUserCreate = React.createClass({
+	getInitialState: function() {
+		return {'username': '', 'device_ID': ''}
+	},
+	updateUserName: function(e) {
+		e.preventDefault()
+		this.setState({'username': e.target.value})
+	},
+	updateDeviceID: function(e) {
+		e.preventDefault()
+		this.setState({'device_ID': e.target.value})
+	},
+	submit: function(e) {
+		e.preventDefault()
+		var form = new FormData()
+		console.log(this.state)
+		form.append("user", JSON.stringify(this.state))
+		$.ajax({
+		    type: 'POST',
+		    url: '/test/insertdb/user',
+		   	contentType: false,
+		   	processData: false,
+		    data: form,
+			success: function(resp) {
+				console.log(resp)
+		    	this.setState({"id": JSON.parse(resp)})
+		    }.bind(this)
+  		});
+	},
+	render: function() {
+		return (
+			<div className="test-user-create-container">
+				<h3 className="test-title">Test User Creation</h3>
+				<form className="form" onSubmit={this.submit}>
+					<input className="person-input-field" type="text" placeholder="username" value={this.state.username} onChange={this.updateUserName}/>
+					<input className="person-input-field" type="text" placeholder="device_ID" value={this.state.device_ID} onChange={this.updateDeviceID}/>
+					<input type="submit" value="Submit" />
+				</form> 
+				<h4>{this.state.id}</h4>
+			</div>
+		)
+	}
+})
 
 ReactDOM.render(<Main />, document.getElementById('content'));
